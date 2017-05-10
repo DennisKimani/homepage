ff10c328ccf7ea149275e111d7895515b9fc85fc
DennisKimani <dennisngugi.dk@gmail.com>
2017-05-10 11:06:20 +0300

    retry

---
+++ js/jquery.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function(){
+jQuery("img").click(function() {
+       alert("THE BIGGEST MARKET FOR TRAVELLING IN COMFORT AND CHEAPER THAN THE REST.CLICK ON GALLERY FOR MORE....NO DISCRIMINATION IN THE VEHICLE.");
+     });
+});
